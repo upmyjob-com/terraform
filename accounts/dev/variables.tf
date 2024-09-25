3fc2678c76f20d079731049fa7032d6430cf71da
roninhockley <roninhockley@gmail.com>
2024-09-25 12:16:27 -0500

    create vpc in dev

---
+++ accounts/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the VPC"
+  type        = string
+}
